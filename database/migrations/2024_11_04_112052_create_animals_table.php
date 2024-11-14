ef36235255d42d209b8a137f36fc42e716a4ff67
Jan Musiol <jasiek.musiol@gmail.com>
2024-11-14 12:08:36 +0100

    connected the database data to the GameController

--- database/migrations/2024_11_04_112052_create_animals_table.php
+++ database/migrations/2024_11_04_112052_create_animals_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('animals', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('short_name');
+            $table->text('initial_hint');
             $table->string('size');
             $table->string('habitat');
             $table->string('diet');
