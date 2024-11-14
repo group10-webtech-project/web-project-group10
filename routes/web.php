6d71cc84ad83b0545fd4c45aec7d38ddc12909bf
KonyakB <bencekanyok@gmail.com>
2024-11-15 00:22:25 +0100

    resolving merge conflict

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,28 @@ use App\Http\Controllers\GameController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('web')->group(function () {
+    // Public routes
+    Route::get('/', [GameController::class, 'index'])->name('game.index');
+    Route::post('/game/new', [GameController::class, 'newGame'])->name('game.new');
+    Route::post('/set-theme', [GameController::class, 'setTheme'])->name('game.setTheme');
+
+    // Rate-limited routes
+    Route::middleware('throttle:30,1')->group(function () {
+        Route::post('/game/guess', [GameController::class, 'guess'])->name('game.guess');
+        Route::post('/game/hint', [GameController::class, 'buyHint'])->name('game.hint');
+        Route::post('/game/check', [GameController::class, 'checkCharacteristic'])->name('game.checkCharacteristic');
+        Route::post('/game/reveal', [GameController::class, 'revealAnswer'])->name('game.reveal');
+        Route::post('/game/give-up', [GameController::class, 'giveUp'])->name('game.giveUp');
+    });
+
+    // Authentication routes
+    Route::get('/register', [RegisterController::class, 'showRegisterForm']);
+    Route::post('/register', [RegisterController::class, 'register']);
+
+    Route::get('/login', [LoginController::class, 'showLoginForm']);
+    Route::post('/login', [LoginController::class, 'login']);
+
+    Route::get('/dashboard', [LoginController::class, 'dashboard'])->middleware('auth');
+    Route::get('/logout', [LoginController::class, 'logout']);
 });
-
-Route::get('/game', [GameController::class, 'index']) ->name('game.index');
-Route::post('/game/guess', [GameController::class, 'guess'])->name('game.guess');
-Route::post('/game/hint', [GameController::class, 'buyHint'])->name('game.hint');
-Route::post('/game/new', [GameController::class, 'newGame'])->name('game.new');
-Route::post('/game/check', [GameController::class, 'checkCharacteristic'])->name('game.check');
-
-Route::get('/register', [RegisterController::class, 'showRegisterForm']);
-Route::post('/register', [RegisterController::class, 'register']);
-
-Route::get('/login', [LoginController::class, 'showLoginForm']);
-Route::post('/login', [LoginController::class, 'login']);
-
-Route::get('/dashboard', [LoginController::class, 'dashboard'])->middleware('auth');
-Route::get('/logout', [LoginController::class, 'logout']);
